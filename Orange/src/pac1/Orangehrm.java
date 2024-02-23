fde9f42c10ca5a671eb1cf844e17b9f5f4484ebf
Rakhi <ak.rakhi89@gmail.com>
2024-02-23 10:11:42 +0530

    new class added

---
+++ Orange/src/pac1/Orangehrm.java
@@ -0,0 +1,46 @@
+package pac1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Orangehrm {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\harisree\\Desktop\\Selenium\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+	   	 WebDriver driver=new FirefoxDriver();
+	   	 
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.get("http://localhost/orangehrm/orangehrm-5.6/orangehrm-5.6/web/index.php/auth/login");
+		
+		driver.findElement(By.name("username")).sendKeys("admin");
+		
+        driver.findElement(By.name("password")).sendKeys("Rakhi@12345");
+        
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+		
+		//driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a")).click();
+		driver.findElement(By.linkText("PIM")).click();
+		
+		 driver.findElement(By.linkText("Add Employee")).click();
+		 
+		 driver.findElement(By.name("firstName")).sendKeys("Rakhi");
+		 
+		 driver.findElement(By.name("lastName")).sendKeys("A K"); 
+		 
+		 driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/input")).sendKeys("001");
+		 
+		 driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]")).click();
+       // driver.close();
+        
+	}
+	
+	
+
+}
